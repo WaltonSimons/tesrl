60cfd7cf93d84b64dc35aca41e1f87cf6bc59a40
WaltonSimons <masteroskar@gmail.com>
2018-05-21 10:23:13 +0200

    Moved map loading to Loader.

--- assets.py
+++ assets.py
@@ -27,21 +27,7 @@ class Assets:
         return self.loader.load_module('main')
 
     def get_map(self, module, folder, name):
-        raw_meta = load(open('modules/{}/maps/{}/{}.yaml'.format(module, folder, name)))
-        raw_map = open('modules/{}/maps/{}/{}.map'.format(module, folder, name))
-        legend = raw_meta.get('map_info').get('legend')
-        legend = {str(key): self.get_asset('tiles', tile_id) for key, tile_id in legend.items()}
-        map_data = raw_map.readlines()
-        map_object = Map(len(map_data), len(map_data[0].strip()))
-        x = 0
-        for line in map_data:
-            y = 0
-            for tile_id in line.strip():
-                map_object.tile_map[y][x].terrain = legend[tile_id]
-                y += 1
-            x += 1
-        map_object.reload_fov_map()
-        return map_object
+        return self.loader.load_map(module, folder, name)
 
     def instantiate_creature(self, creature_template):
         attributes = copy.deepcopy(random.choice(creature_template.base_attributes))
--- loader.py
+++ loader.py
@@ -1,5 +1,5 @@
 from object import TileType, Object
-from maps import Room
+from maps import Room, Map
 import components
 import equipment
 import tcod
@@ -178,3 +178,20 @@ class Loader:
                 room = Room(tile_map, exits)
                 items[room_id] = room
         return items
+
+    def load_map(self, module, folder, name):
+        raw_meta = load(open('modules/{}/maps/{}/{}.yaml'.format(module, folder, name)))
+        raw_map = open('modules/{}/maps/{}/{}.map'.format(module, folder, name))
+        legend = raw_meta.get('map_info').get('legend')
+        legend = {str(key): self.assets_dict['tiles'][tile_id] for key, tile_id in legend.items()}
+        map_data = raw_map.readlines()
+        map_object = Map(len(map_data), len(map_data[0].strip()))
+        x = 0
+        for line in map_data:
+            y = 0
+            for tile_id in line.strip():
+                map_object.tile_map[y][x].terrain = legend[tile_id]
+                y += 1
+            x += 1
+        map_object.reload_fov_map()
+        return map_object
